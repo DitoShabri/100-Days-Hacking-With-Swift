d3fec3ddd0dc50081834b16a9bfed58de1781eb4
DitoShabri <sabrifirli@gmail.com>
2020-12-12 21:23:04 +0700

    Day 2 – arrays, dictionaries, sets, and enums

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,48 +1,120 @@
 import UIKit
 
-//Variable,String And Integer
+//Arrays
 
-var str2 = "Hello, Playground ! "
+let enggar = "EnggarAjip"
+let dito = "DitoShabri"
+let tika = "Fatika"
 
-print(str2)
+let Squad = [enggar,dito,tika]
 
-str2 = "Good bye"
+Squad [1]
 
-print(str2)
+//Set
+
+let colors = Set(["red", "green", "blue"])
+
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+//Tuple
+
+var name = (first: "Firli", last: "Shabri")
+
+name.0
+
+name.last
 
 
+//Array vs set vs tupel
 
 
-var age = 38
-var popualation = 8_000_000
+//If you need a specific, fixed collection of related values where each item has a precise position or name, you should use a
+//tuple:
 
-//Multi Line String
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
 
-var aboutme = """
-My Name is Dito,
-I am junior ios developer,
-From indonesia.
-"""
+//If you need a collection of values that must be unique or you need to be able to check whether a specific item is in there extremely quickly, you should use a set:
 
-//Doubles and booleans
+let set = Set(["aardvark", "astronaut", "azalea"])
 
-var pi = 3.141
-var awesome = true
+//If you need a collection of values that can contain duplicates, or the order of your items matters, you should use an array:
 
-// String interpolation
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
 
-var score = 85
-var str = "Your score was \(score)"
+//Dictionaries
 
-var results = "The test results are here: \(str)"
+let heights = [
+    "Flip Raihart": 1.78,
+    "Batman": 1.73
+]
 
-// Constants
+heights["Batman"]
 
-let dito = "shabri"
+//Dictionary default values
 
-//Type annotations
+let favoriteIceCream = [
+    "Vania": "Chocolate",
+    "Firli": "Vanilla"
+]
+favoriteIceCream["Firli"]
+favoriteIceCream["Charlotte"]
 
-let album: String = "Reputation"
-let year: Int = 1989
-let height: Double = 1.78
-let taylorRocks: Bool = true
+favoriteIceCream["Tono", default: "Unknown"]
+
+//Creating empty collections
+
+var teams = [String: String]()
+
+teams["Paul"] = "Red"
+
+var results9 = [Int]()
+
+var words = Set<String>(
+)
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+
+var result10 = Array<Int>()
+
+//Enumerations
+
+let result0 = "failure"
+
+let result2 = "failed"
+let result3 = "fail"
+
+enum Result {
+    case success
+    case failure
+}
+
+enum Result10 {
+    case success
+    case failure
+}
+
+let result4 = Result10.failure
+
+// Enum associated values
+
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "football")
+
+//let talking = Activity.talking(topic: "football")
+
+
+enum Planet: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+}
+
