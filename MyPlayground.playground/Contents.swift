e9d00c74f451a9ccfaad4463a209f81e88b38cc3
DitoShabri <sabrifirli@gmail.com>
2020-12-12 20:02:06 +0700

    Day 1 – variables, simple data types, and string interpolation

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+//Variable,String And Integer
+
+var str2 = "Hello, Playground ! "
+
+print(str2)
+
+str2 = "Good bye"
+
+print(str2)
+
+
+
+
+var age = 38
+var popualation = 8_000_000
+
+//Multi Line String
+
+var aboutme = """
+My Name is Dito,
+I am junior ios developer,
+From indonesia.
+"""
+
+//Doubles and booleans
+
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+
+var score = 85
+var str = "Your score was \(score)"
+
+var results = "The test results are here: \(str)"
+
+// Constants
+
+let dito = "shabri"
+
+//Type annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
